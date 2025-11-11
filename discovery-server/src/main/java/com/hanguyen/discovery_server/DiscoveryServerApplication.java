e71c8b5eb36f745b08c8467782ef861c13eb4450
havanguyen <hvnguyen902993@gmail.com>
2025-11-11 18:17:46 +0700

    final deploy

--- discovery-server/src/main/java/com/hanguyen/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/hanguyen/discovery_server/DiscoveryServerApplication.java
@@ -2,7 +2,6 @@ package com.hanguyen.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
