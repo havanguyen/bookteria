27f13ce567e496306cae9b4d960687f0c09100c4
havanguyen <hvnguyen902993@gmail.com>
2025-11-16 20:14:26 +0700

    change saga order

--- payment-service/src/main/java/com/hanguyen/payment_service/service/VNPayService.java
+++ payment-service/src/main/java/com/hanguyen/payment_service/service/VNPayService.java
@@ -42,7 +42,7 @@ public class VNPayService {
         vnp_Params.put("vnp_ReturnUrl", vnPayConfig.getReturnUrl());
         vnp_Params.put("vnp_IpAddr", ipAddress);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
